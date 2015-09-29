56f0f28c722bf1ecd6ef7bb1b2b04e535c923b51
Jason Wagner <info@jasonwagnerdesign.com>
2015-09-29 15:53:03 -0600

    wag-1 repeater portfolio

--- wp-content/themes/wagcodes/front-page.php
+++ wp-content/themes/wagcodes/front-page.php
@@ -1,3 +1,36 @@
+
+
 <section id="portfolio">
-	<h1>Jason's Portfolio</h1>
+	<h1>Portfolio</h1>
+
+	<?php if(have_rows('portfolio')): ?>
+		<?php while (have_rows('portfolio')) : the_row(); ?>
+			<div>
+				<?php 
+					$title = get_sub_field('title');
+					$description = get_sub_field('description');
+					$url = get_sub_field('url');
+					$image = get_sub_field('image');
+				?>
+				<?php if($title):?>
+					<h2><?php echo $title;?></h2>
+				<?php endif;?>
+
+				<?php if($description):?>
+					<p><?php echo $description;?></p>
+				<?php endif;?>
+
+				<?php if($image):?>
+					<img src="<?php echo $image;?>" alt="<?php echo $title;?>"/>
+				<?php endif;?>
+
+				<?php if($url):?>
+					<a href="<?php echo $url;?>">Visit the Site</a>
+				<?php endif;?>
+
+			</div>
+		<?php endwhile; ?>
+	<?php endif; ?>
+
+
 </section>
--- wp-content/themes/wagcodes/templates/header.php
+++ wp-content/themes/wagcodes/templates/header.php
@@ -4,7 +4,7 @@
 			<a href="mailto:wagcodes@gmail.com?subject=Let's Create a Website"><?php include get_theme_root() . '/wagcodes/_/build/svg/icon-email.svg'; ?></a>
 			<a href="https://www.linkedin.com/pub/jason-wagner/b/408/641" target="_blank"><?php include get_theme_root() . '/wagcodes/_/build/svg/icon-linkedin.svg'; ?></a>
 		</nav>
-		
+
 		<div>
 			<h1>Jason </br>Wagner</h1>
 			<p>I'm a Web Developer specializing in Wordpress, Laravel, PHP, JavaScript and CSS. When I'm not in front of a computer, you can find me in the woods, practicing yoga, playing music, or hanging with my dog Bufford. </p>
@@ -17,7 +17,7 @@
 			<?php include get_theme_root() . '/wagcodes/_/build/svg/icon-paw.svg'; ?>
 		</div>
 
-		<a href="#"><?php include get_theme_root() . '/wagcodes/_/build/svg/icon-down.svg'; ?>Scroll to see the goods</a>
+		<a href="#portfolio" id="jump-portfolio"><?php include get_theme_root() . '/wagcodes/_/build/svg/icon-down.svg'; ?>Scroll to see the goods</a>
 
 
 	</header>
